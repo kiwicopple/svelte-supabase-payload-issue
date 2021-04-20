a9fb745c167252ea720ef2c139d654a92c576fb1
Paul Copplestone <pcopplestone@gmail.com>
2021-04-20 19:52:03 +0800

    Fromat the code to be a little more readable

--- src/lib/Test.svelte
+++ src/lib/Test.svelte
@@ -1,48 +1,52 @@
 <script>
-  import { onMount, afterUpdate } from 'svelte';
-  import { writable } from 'svelte/store';
+  import { onMount, afterUpdate } from "svelte";
+  import { writable } from "svelte/store";
 
-  import { createClient } from '@supabase/supabase-js';
+  import { createClient } from "@supabase/supabase-js";
 
   export let env = import.meta.env;
 
-  let table = 'items';
+  let table = "items";
 
   export const supabase = createClient(
-      env.VITE_SUPABASE_URL, 
-      env.VITE_SUPABASE_KEY
+    env.VITE_SUPABASE_URL,
+    env.VITE_SUPABASE_KEY
   );
 
   let fetchItems = async () => {
-    var {data, error} = {};
-    var { data, error } = await supabase
-      .from(table).select()
-    console.log(`Fetched items - `, data)
-    if ( !error ) { return data; } else { throw error; }
+    var { data, error } = {};
+    var { data, error } = await supabase.from(table).select();
+    console.log(`Fetched items - `, data);
+    if (!error) {
+      return data;
+    } else {
+      throw error;
+    }
   };
 
-  let items = writable([])
+  let items = writable([]);
 
   let updateItems = async () => {
-    let updatedItems = await fetchItems()
-      .then(results => { console.log(`Updated items - `, results); items.set(results)});
+    let updatedItems = await fetchItems().then((results) => {
+      console.log(`Updated items - `, results);
+      items.set(results);
+    });
   };
 
   const itemsSub = supabase
     .from(table)
-    .on('*', async () => {
+    .on("*", async () => {
       try {
-        updateItems()
-      } 
-      catch(error) {
-        throw(error);
-      }})
+        updateItems();
+      } catch (error) {
+        throw error;
+      }
+    })
     .subscribe();
-  
+
   onMount(() => {
     updateItems();
   });
-
 </script>
 
 {#each $items as item}
